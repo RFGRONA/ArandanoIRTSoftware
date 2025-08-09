4e1eb064df09dcbae25a8c1c8ca75f807fef470d
Gabe <gabrielemartinez@ucundinamarca.edu.co>
2025-08-09 15:38:22 -0500

    fix: session user actions and information

---
+++ 3_Presentation/ViewComponents/UserInfoViewComponent.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ArandanoIRT.Web._0_Domain.Entities; 
+
+namespace ArandanoIRT.Web._3_Presentation.ViewComponents
+{
+    public class UserInfoViewComponent : ViewComponent
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public UserInfoViewComponent(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (!_signInManager.IsSignedIn(UserClaimsPrincipal))
+            {
+                return Content(string.Empty);
+            }
+
+            var user = await _userManager.GetUserAsync(UserClaimsPrincipal);
+            if (user == null)
+            {
+                return Content("Usuario no encontrado.");
+            }
+
+            var userRole = User.IsInRole("Admin") ? "Administrador" : "Usuario";
+            var model = (Name: $"{user.FirstName} {user.LastName}", Role: userRole);
+
+            return View(model);
+        }
+    }
+}
