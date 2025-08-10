1de9fef61fe1ea95cc3b515ba27a6bab63ed205b
Gabe <gabrielemartinez@ucundinamarca.edu.co>
2025-08-10 02:23:11 -0500

    fix: duplication in date conversion

--- 0_Domain/Common/QueryableExtensions.cs
+++ 0_Domain/Common/QueryableExtensions.cs
@@ -1,40 +1,39 @@
 using System.Linq.Expressions;
-using ArandanoIRT.Web._1_Application.Services.Contracts;
+using ArandanoIRT.Web._1_Application.Services.Contracts; 
 
-namespace ArandanoIRT.Web._0_Domain.Common;
-
-public static class QueryableExtensions
+namespace ArandanoIRT.Web._0_Domain.Common
 {
-    public static IQueryable<T> ApplyDateFilters<T>(
-        this IQueryable<T> query,
-        DataQueryFilters filters,
-        Expression<Func<T, DateTime>> dateSelector)
+    public static class QueryableExtensions
     {
-        var parameter = dateSelector.Parameters[0];
-        var colombiaTimeZone = TimeZoneInfo.FindSystemTimeZoneById("America/Bogota");
-
-        if (filters.StartDate.HasValue)
+        public static IQueryable<T> ApplyDateFilters<T>(
+            this IQueryable<T> query,
+            DataQueryFilters filters,
+            Expression<Func<T, DateTime>> dateSelector)
         {
-            // Trata la fecha de inicio como el comienzo del día en Colombia y conviértela a UTC
-            var startDateUnspecified = filters.StartDate.Value.Date;
-            var startDateUtc = TimeZoneInfo.ConvertTimeToUtc(startDateUnspecified, colombiaTimeZone);
 
-            var startDateBody = Expression.GreaterThanOrEqual(dateSelector.Body, Expression.Constant(startDateUtc));
-            var startDateLambda = Expression.Lambda<Func<T, bool>>(startDateBody, parameter);
-            query = query.Where(startDateLambda);
+            if (filters.StartDate.HasValue)
+            {
+                var startDateUtc = filters.StartDate.Value;
+                
+                var startPredicate = Expression.Lambda<Func<T, bool>>(
+                    Expression.GreaterThanOrEqual(dateSelector.Body, Expression.Constant(startDateUtc)),
+                    dateSelector.Parameters
+                );
+                query = query.Where(startPredicate);
+            }
+
+            if (filters.EndDate.HasValue)
+            {
+                var endDateUtc = filters.EndDate.Value;
+                
+                var endPredicate = Expression.Lambda<Func<T, bool>>(
+                    Expression.LessThanOrEqual(dateSelector.Body, Expression.Constant(endDateUtc)),
+                    dateSelector.Parameters
+                );
+                query = query.Where(endPredicate);
+            }
+
+            return query;
         }
-
-        if (filters.EndDate.HasValue)
-        {
-            // Trata la fecha de fin como el final del día en Colombia y conviértela a UTC
-            var endDateUnspecified = filters.EndDate.Value.Date.AddDays(1);
-            var endDateUtc = TimeZoneInfo.ConvertTimeToUtc(endDateUnspecified, colombiaTimeZone);
-
-            var endDateBody = Expression.LessThan(dateSelector.Body, Expression.Constant(endDateUtc));
-            var endDateLambda = Expression.Lambda<Func<T, bool>>(endDateBody, parameter);
-            query = query.Where(endDateLambda);
-        }
-
-        return query;
     }
 }
