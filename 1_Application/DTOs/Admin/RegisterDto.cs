44ac1303990880654d2364e3e0bf6c269ef59739
Juan-qwerty <Juahddh@gmail.com>
2025-08-17 15:42:02 -0500

    fix: translations of system error messages, for better control of the data being sent

--- 1_Application/DTOs/Admin/RegisterDto.cs
+++ 1_Application/DTOs/Admin/RegisterDto.cs
@@ -4,23 +4,28 @@ namespace ArandanoIRT.Web._1_Application.DTOs.Admin;
 
 public class RegisterDto
 {
-    [Required]
+    [Required(ErrorMessage = "El campo \"{0}\" es obligatorio.")]
     [Display(Name = "Código de Invitación")]
     public string InvitationCode { get; set; } = string.Empty;
 
-    [Required][EmailAddress] public string Email { get; set; } = string.Empty;
+    [Required(ErrorMessage = "El campo \"{0}\" es obligatorio.")]
+    [EmailAddress(ErrorMessage = "El campo \"{0}\" no es una dirección de correo electrónico válida.")]
+    public string Email { get; set; } = string.Empty;
 
-    [Required][Display(Name = "Nombre")] public string FirstName { get; set; } = string.Empty;
+    [Required(ErrorMessage = "El campo \"{0}\" es obligatorio.")]
+    [Display(Name = "Nombre")] 
+    public string FirstName { get; set; } = string.Empty;
 
-    [Required]
+    [Required(ErrorMessage = "El campo \"{0}\" es obligatorio.")]
     [Display(Name = "Apellido")]
     public string LastName { get; set; } = string.Empty;
 
-    [Required]
+    [Required(ErrorMessage = "El campo \"{0}\" es obligatorio.")]
     [DataType(DataType.Password)]
     [Display(Name = "Contraseña")]
     public string Password { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "El campo \"{0}\" es obligatorio.")]
     [DataType(DataType.Password)]
     [Display(Name = "Confirmar Contraseña")]
     [Compare("Password", ErrorMessage = "La contraseña y la confirmación no coinciden.")]
